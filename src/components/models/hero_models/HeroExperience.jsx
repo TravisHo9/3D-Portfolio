51e5982c913a2caa51434cdc8a2948c8d6fd5ea7
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2025-07-11 15:50:49 -0700

    Add Room component to implement 3D model of room and adjust directional light intensity in HeroExperience

--- src/components/models/hero_models/HeroExperience.jsx
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
 import { OrbitControls } from "@react-three/drei";
+import { Room } from "./Room.jsx";
 
 const HeroExperience = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
@@ -10,7 +11,7 @@ const HeroExperience = () => {
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       {/* deep blue ambient */}
       <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={5} />
+      <directionalLight position={[5, 5, 5]} intensity={2} />
       <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
@@ -20,10 +21,7 @@ const HeroExperience = () => {
         maxPolarAngle={Math.PI / 2}
       />
 
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial color="teal" />
-      </mesh>
+    <Room />
     </Canvas>
   );
 };
