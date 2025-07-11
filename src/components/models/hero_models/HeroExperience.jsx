9390131aac079c00f0eb755b3ee55defe83c89dd
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2025-07-11 15:41:40 -0700

    Add initial project structure with images, models, and components
    - Added various image assets including logos and icons.
    - Introduced 3D models for computer, Python, React, and more.
    - Created main application structure with App component.
    - Implemented Hero section with dynamic text and button.
    - Developed Button component for smooth scrolling functionality.
    - Integrated HeroExperience component for 3D visualization.
    - Set up Vite configuration for React and Tailwind CSS.
    - Added global styles in index.css for consistent theming.

---
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -0,0 +1,31 @@
+import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+import { OrbitControls } from "@react-three/drei";
+
+const HeroExperience = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+
+  return (
+    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+      {/* deep blue ambient */}
+      <ambientLight intensity={0.2} color="#1a1a40" />
+      <directionalLight position={[5, 5, 5]} intensity={5} />
+      <OrbitControls
+        enablePan={false}
+        enableZoom={!isTablet}
+        maxDistance={20}
+        minDistance={5}
+        minPolarAngle={Math.PI / 5}
+        maxPolarAngle={Math.PI / 2}
+      />
+
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color="teal" />
+      </mesh>
+    </Canvas>
+  );
+};
+
+      export default HeroExperience;
