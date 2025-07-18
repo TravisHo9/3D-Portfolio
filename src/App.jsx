58a020dde6f0744ca0152c0629c95395eb868779
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2025-07-18 15:33:10 -0700

    Add NavBar, LogoSection, FeatureCards, and ShowcaseSection components; update App component to include new sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import LogoSection  from "./components/LogoSection";
+import NavBar from "./components/NavBar";
+import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
 
 const App = () => (
   <>
+    <NavBar />
     <Hero />
+    <ShowcaseSection />
+    <LogoSection />
+    <FeatureCards />
   </>
 );
 
